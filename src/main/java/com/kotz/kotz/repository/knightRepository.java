564ff04f0b888c2eaf6a7a429e2ee1ab18a31656
Enzo Arauco <enzoarauco@gmail.com>
2020-07-27 11:31:43 -0500

    add connection to postreSQL

--- src/main/java/com/kotz/kotz/repository/knightRepository.java
+++ src/main/java/com/kotz/kotz/repository/knightRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface knightRepository extends JpaRepository<knight, Long> {
 
-    @Query(value = "SELECT id_knight, knight.name, constellation, url_photo, hability1, hability2, hability3, hability4, armor_id_armor, god_id_god from " +
+    @Query(value = "SELECT id_knight, knight.name, constellation, knight.url_photo, hability1, hability2, hability3, hability4, armor_id_armor, god_id_god from " +
             "knight INNER JOIN god on knight.god_id_god = god.id_god where god.name = :name", nativeQuery = true)
     List<knight> findKnightByGod(@Param("name") String name);
 
@@ -18,10 +18,8 @@ public interface knightRepository extends JpaRepository<knight, Long> {
             "knight INNER JOIN type_armor on knight.armor_id_armor = type_armor.id_armor where type_armor.name_armor = :armor", nativeQuery = true)
     List<knight> findKnightByArmor(@Param("armor") String armor);
 
-    @Query(value = "SELECT * FROM knight ORDER BY rand() LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM knight ORDER BY random() LIMIT 1", nativeQuery = true)
+    //@Query(value = "SELECT * FROM knight ORDER BY rand() LIMIT 1", nativeQuery = true)
     knight getRandomKnight();
-
-    /*SELECT * from knight INNER JOIN god on knight.god_id_god = god.id_god where god.name = 'Athena'
-SELECT id_knight, name, constellation, hability1, hability2, hability3, hability4 from knight INNER JOIN god on knight.god_id_god = god.id_god where god.name = 'Athena'*/
 
 }
